57e75634616512232e49f2f9fd09afb288b59ed7
bitry <tozika@inbox.ru>
2016-10-30 19:52:40 +0300

    change registration form

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'navbar',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
-  
+    actions = new EventEmitter<string>();
+  closeModel() {
+    this.actions.emit("closeModal");
+  }
  }
