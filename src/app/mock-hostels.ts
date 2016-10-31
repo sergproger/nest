a5deea4e30119084194511435cfcfc30fd3d9105
sergproger <sergmilan94@tut.by>
2016-10-31 22:50:45 +0300

    Modify mock objects

---
+++ src/app/mock-hostels.ts
@@ -0,0 +1,10 @@
+import { Hostel } from './hostel';
+
+export const HEROES: Hostel[] = [
+  {id: 1, nameHostel: 'Mr. Nice',descr:'Хостел в питере около метро',stars:3,price:600},
+  {id: 2, nameHostel: 'Narco',descr:'Хостел в питере 5 минут от метро',stars:4,price:1000},
+  {id: 3, nameHostel: 'Bombasto',descr:'Хостел в Санкт-Питербурге',stars:2,price:200},
+  {id: 4, nameHostel: 'Celeritas',descr:'Хостел ...',stars:3,price:500},
+  {id: 5, nameHostel: 'Magneta',descr:'Хостел круче всех',stars:4,price:700},
+  {id: 16, nameHostel: 'RubberMan',descr:'Хостел просто хостел',stars:1,price:1020},
+];
