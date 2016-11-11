f9927bbc626be0e7928285192dcd5c16a1a9fd00
Serghey <sergmilan94@tut.by>
2016-11-11 14:41:35 +0300

    add in work component detail

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
+import { Contact } from './contact-detail/contact';
+import { ContactService } from './contact.service';
 
 @Component({
   selector: 'contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers: [ContactService]
 })
-export class ContactsComponent { }
+export class ContactsComponent implements OnInit{
+  contacts: Contact[];
+  constructor(private contactService: ContactService) { }
+
+  getContacts(): void {
+    this.contactService.getContacts().then(contacts => this.contacts = contacts);
+  }
+
+  ngOnInit(): void {
+    this.getContacts();
+  }
+
+}
