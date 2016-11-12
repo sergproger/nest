354f680845b9df8e2f9354b4dc7d1baa4912ca95
tozik <tozika@inbox.ru>
2016-11-12 17:07:28 +0300

    add entrance component

---
+++ src/app/entrance/entrance.component.ts
@@ -0,0 +1,15 @@
+import { Component,EventEmitter } from '@angular/core';
+import {MaterializeAction} from "angular2-materialize";
+
+@Component({
+  selector: 'entrance',
+  templateUrl: './entrance.component.html',
+  styleUrls: ['./entrance.component.css']
+})
+export class EntranceComponent {
+
+   modalActions = new EventEmitter<string|MaterializeAction>();
+  openModal() {
+    this.modalActions.emit({action:"modal",params:['open']});
+  }
+ }
