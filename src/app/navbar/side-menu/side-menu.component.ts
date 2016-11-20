9402afb2f81ac8fcff9184164a557c9a4a989a5d
Anton <jt.11@mail.ru>
2016-11-20 15:38:23 +0300

    close sidenav after click

--- src/app/navbar/side-menu/side-menu.component.ts
+++ src/app/navbar/side-menu/side-menu.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
+
+  AboutClick() {
+    $('.button-collapse').sideNav('hide');
+  }
+  HostelsClick(){
+    $('.button-collapse').sideNav('hide');
+  }
+  ContactsClick(){
+    $('.button-collapse').sideNav('hide');
+  }
 }
